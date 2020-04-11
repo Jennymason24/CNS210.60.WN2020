0d7c3fbec1f225f9631d53d50b1493cefd51d32d
Jennymason24 <masonjennifea01@my.jccmi.edu>
2020-04-11 13:15:53 -0400

    recursive

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,12 +1,10 @@
 def fibonacci(n):
-    F0=0
-    F1=1
     if n < 0:
         print("Incorrect input")
     elif n == 0:
-        return F0
+        return 0
     elif n == 1:
-        return F1
+        return 1
     else:
         return
 Fibonacci(n-1)+Fibonacci(n-2)
