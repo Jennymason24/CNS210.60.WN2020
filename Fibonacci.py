faecf39235688197a34b2b10610464a4463b068f
Jennymason24 <masonjennifea01@my.jccmi.edu>
2020-04-11 15:19:38 -0400

    updated

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,3 +1,4 @@
+import argparse
 def fibonacci(n):
     if n < 0:
         print("Incorrect input")
@@ -6,8 +7,33 @@ def fibonacci(n):
     elif n == 1:
         return 1
     else:
-        return
-Fibonacci(n-1)+Fibonacci(n-2)
+        return fibonacci(n-1)+fibonacci(n-2)
    
-print(fibonacci(9))
 
+
+def main():
+    parser_formatter = argparse.ArgumentDefaultsHelpFormatter
+    parser = argparse.ArgumentParser(
+            description = 'Packet Analysis Study Quiz (TODO: better description)',
+            formatter_class=parser_formatter)
+
+    parser.add_argument('--filename', required=True, action='store',
+            dest='filename', help='pcap file used to quiz')
+
+    parser.add_argument('--number', required=False, action='store',
+            dest='number', type=int, default=True, help='Displays Header Reference for question')
+  
+
+    args = parser.parse_args()
+
+    print(args.filename, args.number)
+    
+    output=fibonacci(args.number)
+    print(output)
+
+    f = open(args.filename, "w")
+    f.write(str(output))
+    f.close()
+
+if __name__ == '__main__':
+    main()
