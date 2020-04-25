932fecf7b3a6dc880b6278bcf85e9942ece2dbea
Jennymason24 <masonjennifea01@my.jccmi.edu>
2020-04-25 09:36:21 -0400

    new

---
+++ usingSSL.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+# import libraries needed
+import ssl
+import socket
+
+# connect on IPv4, TCP
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# use SSL wrapper
+ssock = ssl.wrap_socket(s)
+
+try:
+    # attempt to connect to Google on HTTPS
+    ssock.connect(("www.google.com", 443))
+    print(ssock.cipher())
+except:
+    print("error")
+
+try:
+    # Issue GET request
+    ssock.write(b"GET/ \r\n")
+except: Exception as e:
+    print("write error: ", e)
+
+# save data 
+data = bytearray()
+try:
+    data = ssock.read()
+except Exception as e:
+    print("read error: ", e)
+
+# print data 
+print(data.decode("utf-8"))
